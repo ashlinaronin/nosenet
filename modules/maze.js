f82f75572c57dee32f4e353f94706f178a781eb8
ashlinaronin <ashlinaronin@gmail.com>
2018-06-18 12:39:12 -0400

    Define map based on points

--- modules/maze.js
+++ modules/maze.js
@@ -1,7 +1,24 @@
-export function drawMaze(ctx, videoWidth, videoHeight, color) {
-  ctx.beginPath();
-  ctx.arc(videoWidth / 2, videoHeight / 2, 70, 0, Math.PI * 2, true);
+export function generateMap(width, height) {
+  return [
+    [0,0],
+    [width/2, height/2],
+    [width, height]
+  ]
+}
 
+export function drawMaze(ctx, videoWidth, videoHeight, color) {
+  const map = generateMap(videoWidth, videoHeight);
+  const [ x, y ] = map[0];
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  for (let i = 1; i < map.length; i++) {
+    const [ pointX, pointY ] = map[i];
+    ctx.lineTo(pointX, pointY);
+  }
+
+  ctx.closePath();
   ctx.strokeStyle = color;
   ctx.lineWidth = 15;
   ctx.stroke();
