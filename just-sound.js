a36a825e902d5150d488c677ffc4ddc8dc7a0fab
ashlinaronin <ashlinaronin@gmail.com>
2018-08-16 21:29:34 -0500

    Test out metalSynth

--- just-sound.js
+++ just-sound.js
@@ -1,3 +1,6 @@
-import { playTestPattern } from './modules/newSynth';
+import { playTestPattern, stopSound, playSound } from './modules/newSynth';
+
+document.getElementById('stop-sound').addEventListener('click', stopSound);
+document.getElementById('play-sound').addEventListener('click', playSound);
 
 playTestPattern();
--- modules/newSynth.js
+++ modules/newSynth.js
@@ -1,29 +1,44 @@
 import Tone from 'tone';
 
-const fmSynth = new Tone.FMSynth({
-  "modulationIndex" : 1.22,
-  "envelope" : {
-    "attack" : 0.1,
-    "decay" : 0.2
-  },
-  "modulation" : {
-    "type" : "sine"
-  },
-  "modulationEnvelope" : {
-    "attack" : 0.4,
-    "decay" : 0.05
-  }
+const metalSynth = new Tone.MetalSynth({
+  frequency: 80,
+  envelope:
+    {
+      attack: 0.001,
+      decay:
+        1.4,
+      release:
+        0.9
+    }
+  ,
+  harmonicity: 2.1,
+  modulationIndex:
+    18,
+  resonance:
+    150,
+  octaves:
+    3.5
 }).toMaster();
 
+const harmonicityPattern = new Tone.Pattern((time, harmonicity) => {
+  metalSynth.harmonicity = harmonicity;
+  metalSynth.triggerAttackRelease(0.25, time, 0.8);
+}, [1, 2, 3, 4]);
+
 export function playTestNote() {
-  fmSynth.triggerAttackRelease(80, '32n', Tone.now(), 0.8);
+  metalSynth.triggerAttackRelease(80, '32n', Tone.now(), 0.8);
 }
 
 export function playTestPattern() {
   Tone.Transport.cancel();
-  const pattern = new Tone.Pattern((time, note) => {
-    fmSynth.triggerAttackRelease(note, 0.25);
-  }, ['c2', 'e2', 'g2', 'a2']);
-  pattern.start();
+  harmonicityPattern.start();
+  Tone.Transport.start();
+}
+
+export function stopSound() {
+  Tone.Transport.stop();
+}
+
+export function playSound() {
   Tone.Transport.start();
 }
